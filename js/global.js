017e840df2e8a928c7d363e05c7798fcdc14a672
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2024-03-24 23:21:47 +0100

    Galery, small changes

--- js/global.js
+++ js/global.js
@@ -8,3 +8,7 @@ function hideHamburgerMenu() {
     var x = document.getElementById("hamburger-menu");
     x.style.display = "none";
 }
+function changeImg(src) {
+    var img = document.getElementById("main-img");
+    img.src = src;
+}
