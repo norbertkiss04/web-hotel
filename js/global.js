ecff85a645c795052f10ebd94ac7ca46497fab28
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2024-03-24 22:35:35 +0100

    Responsive navbar, small changes

---
+++ js/global.js
@@ -0,0 +1,10 @@
+function showHamburgerMenu() {
+    console.log("show")
+    var x = document.getElementById("hamburger-menu");
+    x.style.display = "block";
+}
+
+function hideHamburgerMenu() {
+    var x = document.getElementById("hamburger-menu");
+    x.style.display = "none";
+}
